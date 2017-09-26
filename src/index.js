969695166819a27408abc9e7f605977f68958cd0
Fabian Vilers <fabian.vilers@dev-one.com>
2017-09-26 17:16:36 +0200

    Add Material UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ThemedApp = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(<ThemedApp />, document.getElementById('root'));
 registerServiceWorker();
