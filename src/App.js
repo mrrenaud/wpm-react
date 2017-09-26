ff27363487791b6ab3e6f189c36fed320a9f00f9
Fabian Vilers <fabian.vilers@dev-one.com>
2017-09-26 17:18:31 +0200

    Add an AppBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <AppBar title="WPM" />
       </div>
     );
   }
