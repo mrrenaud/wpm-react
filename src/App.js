810f4db0e1327dfa28dc7595136b5a1a21c8292c
Fabian Vilers <fabian.vilers@dev-one.com>
2017-09-26 17:53:25 +0200

    Add WPM counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import './App.css';
@@ -8,6 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      started: false,
       source: null,
       errorText: null,
       value: null
@@ -23,37 +24,55 @@ class App extends Component {
     }
   }
 
-  componentWillMount() {
-    const sample = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis dignissim lorem. Ut ullamcorper nisl sed semper pulvinar. Aenean varius tempor augue, at tristique erat. Mauris fringilla nisi ut mi tincidunt scelerisque. Morbi porttitor massa sed nibh mollis, id blandit tortor fermentum. Aenean fringilla vitae erat quis euismod. In id hendrerit sem, id imperdiet ante. Etiam a dapibus urna. Mauris dictum neque non est auctor dapibus. Fusce sit amet nunc orci. Vivamus est sapien, facilisis eu faucibus ut, aliquam at magna. Nullam non metus pretium, sagittis nisi quis, vehicula ligula. Nulla congue dolor orci. Integer id ante at ex congue condimentum a a velit. Ut fringilla ligula sit amet mi lobortis, a mattis justo facilisis.';
+  start() {
+    if (this.state.start) {
+      throw new Error('Already started.');
+    }
+
+    const sample = `Le prix Nobel est une récompense internationale décernée chaque année à des personnes ayant apporté un " grand bénéfice à l'humanité ". Ce prix porte le nom du chimiste suédois Alfred Nobel, qui a légué toute sa fortune pour la création de ce prix. Il existe en réalité plusieurs prix, car cette récompense est remise dans 5 domaines différents : la physique, la chimie, la médecine, la littérature et la paix. Les personnes qui reçoivent un prix Nobel se sont illustrées dans leur discipline grâce à une invention, une découverte, un travail... Le premier prix Nobel a été remis en 1901. Depuis, les prix sont décernés tous les ans, au mois d'octobre. La remise des prix a lieu le jour anniversaire de la mort d'Alfred Nobel, le 10 décembre.`;
 
     this.setState({
+      started: true,
+      startedAt: new Date(),
       source: sample,
       sourceLength: sample.length,
       value: '',
       valueLength: 0
-    })
+    });
   }
 
   render() {
+    if (this.state.started) {
+      const words = this.state.value.split(' ').filter(w => w).length;
+      const ellapsed = new Date() - this.state.startedAt;
+      const wpm = Math.round(words / ellapsed * 60000);
+
+      return (
+        <div>
+          <Paper className="sample">
+            <h1>Sample Text</h1>
+            <p>{this.state.source}</p>
+          </Paper>
+          <Paper className="sample">
+            <h1>Type in the text</h1>
+            <TextField
+              autoFocus={true}
+              errorText={this.state.errorText}
+              floatingLabelText={`${this.state.valueLength}/${this.state.sourceLength} (${wpm} wpm)`}
+              fullWidth={true}
+              multiLine={true}
+              onChange={(event, value) => this.onChange(value)}
+              rows={4}
+              value={this.state.value}
+            />
+          </Paper>
+        </div>
+      );
+    }
+
     return (
       <div>
-        <AppBar title="WPM" />
-        <Paper className="sample">
-          <h1>Sample Text</h1>
-          <p>{this.state.source}</p>
-        </Paper>
-        <Paper className="sample">
-          <h1>Type in the text</h1>
-          <TextField
-            errorText={this.state.errorText}
-            floatingLabelText={`${this.state.valueLength}/${this.state.sourceLength}`}
-            fullWidth={true}
-            multiLine={true}
-            onChange={(event, value) => this.onChange(value)}
-            rows={4}
-            value={this.state.value}
-          />
-        </Paper>
+        <RaisedButton label="Start !" onClick={() => this.start()} primary={true} />
       </div>
     );
   }
